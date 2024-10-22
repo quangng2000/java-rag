3270c167eff4c5e5989434da76a5f48e23c8ecc7
Dan Vega <danvega@gmail.com>
2024-10-22 15:46:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring AI RAG
+
+[Spring Initializer](https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.3.4&packaging=jar&jvmVersion=23&groupId=dev.danvega&artifactId=markets&name=markets&description=Demo%20project%20for%20Spring%20Boot&packageName=dev.danvega.markets&dependencies=web,spring-ai-openai,spring-ai-pdf-document-reader,spring-ai-vectordb-pgvector,docker-compose)
---
+++ src/main/java/dev/danvega/markets/ChatController.java
@@ -0,0 +1,27 @@
+package dev.danvega.markets;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder, VectorStore vectorStore) {
+        this.chatClient = builder
+                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
+                .build();
+    }
+
+    @GetMapping("/")
+    public String chat() {
+        return chatClient.prompt()
+                .user("How did the Federal Reserve's recent interest rate cut impact various asset classes according to the analysis")
+                .call()
+                .content();
+    }
+}
